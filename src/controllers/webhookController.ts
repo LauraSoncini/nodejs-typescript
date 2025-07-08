c31289db9e6f99a36d84bcceeec7623acb4c9fd2
LauraSoncini <109616391+LauraSoncini@users.noreply.github.com>
2025-07-08 00:09:15 -0300

    Create webhookController.ts

---
+++ src/controllers/webhookController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'avhealth2025-whatsapp';
+
+export const verifyWebhook = (req: Request, res: Response) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+};
+
+export const handleWebhook = (req: Request, res: Response) => {
+  console.log('🔔 Evento recebido:', JSON.stringify(req.body, null, 2));
+  return res.sendStatus(200);
+};
